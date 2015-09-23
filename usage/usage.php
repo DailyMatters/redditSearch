47e48eaebe6b47ceaf198bf61b74c26b8ee730bc
Cláudio Ribeiro <claudio.santos.ribeiro@gmail.com>
2015-09-23 21:35:57 +0100

    Added usage example

---
+++ usage/usage.php
@@ -0,0 +1,20 @@
+<?php
+
+//use redditSearch\Searcher\Searcher;
+include_once( '../src/Searcher.php' );
+
+//Calling the storeSearch function search for 'entrepreneur', the options are not yet implemented
+storeSearch( 'entrepreneur', 'something' );
+
+/**
+* This function will execute a search and store the result in a file called data.txt
+*/
+function storeSearch( $query, $options ){
+
+	$search = new Searcher();
+	$result = $search->execSearch( $query, $options );
+
+	file_put_contents( 'data.txt', $result, FILE_APPEND);
+
+	echo '*** SEARCH COMPLETE ***';
+}
