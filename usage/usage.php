f14bbed15fafde8fcf8e534900495c1db476218d
Cláudio Ribeiro <claudio.santos.ribeiro@gmail.com>
2015-09-24 20:21:01 +0100

    Testing json_encode

--- usage/usage.php
+++ usage/usage.php
@@ -1,6 +1,7 @@
 <?php
 
-//use redditSearch\Searcher\Searcher;
+use redditSearch\Searcher\Searcher;
+
 include_once( '../src/Searcher.php' );
 
 //Calling the storeSearch function search for 'entrepreneur', the options are not yet implemented
@@ -10,6 +11,8 @@ storeSearch( 'entrepreneur', 'something' );
 * This function will execute a search and store the result in a file called data.txt
 */
 function storeSearch( $query, $options ){
+
+	echo '*** SEARCHING ***';
 
 	$search = new Searcher();
 	$result = $search->execSearch( $query, $options );
